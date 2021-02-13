e2107338e160f2952ee88229ce35dff22c25c6a6
Vitorlivi <vitorlivi2016@hotmail.com>
2021-02-13 13:53:37 -0300

    styled-components no next

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import '../styles/globals.css'
 import { storeWrapper } from '../Redux/Store';
 import './global.css';
 
-function MyApp({ Component, pageProps }) {
+
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
